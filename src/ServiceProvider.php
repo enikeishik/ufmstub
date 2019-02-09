4c1acdab19c17a858d9d1c786caaceda4176014c
enikeishik <enikeishik@gmail.com>
2019-02-09 14:40:11 +0300

    feat: service provider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * UFO Framework Stub module.
+ * 
+ * @copyright   Copyright (C) 2018 - 2019 Enikeishik <enikeishik@gmail.com>. All rights reserved.
+ * @author      Enikeishik <enikeishik@gmail.com>
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+namespace Ufo\Modules\Enikeishik\Ufmstub;
+
+use Ufo\Modules\ServiceProvider as BaseServiceProvider;
+
+class ServiceProvider extends BaseServiceProvider
+{
+}
